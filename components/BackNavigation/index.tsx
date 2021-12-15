29eb931831328a3ee1c0a2840f13de67b2190584
Eskild Mageli <eskild.emedd33@gmail.com>
2021-12-15 21:15:46 +0100

    remove mui

--- components/BackNavigation/index.tsx
+++ components/BackNavigation/index.tsx
@@ -1,12 +1,10 @@
 import style from "./style.module.css";
-import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 import Link from "next/link";
 type BackNavigationProp = { href: string };
 const BackNavigation: React.FC<BackNavigationProp> = ({ href }) => {
   return (
     <Link href={href}>
       <a className={style.container}>
-        <KeyboardBackspaceIcon />
         <span>back</span>
       </a>
     </Link>
