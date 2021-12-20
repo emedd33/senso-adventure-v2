8f79dfa8dbfa529bd88378dc92259bf8657abc3f
Eskild Mageli <eskild.emedd33@gmail.com>
2021-12-20 23:59:20 +0100

    add github footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,9 +1,34 @@
 import React from "react";
 import styles from "./style.module.css";
+import Image from "next/image";
+import Link from "next/link";
 interface FooterProps {}
 
 const Footer: React.FC<FooterProps> = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <span
+        style={{
+          color: "gray",
+          fontFamily: "sans-serif",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <span style={{ marginRight: "1rem" }}>@2022 Eskild Ruud Mageli</span>
+        <Link href="https://github.com/emedd33/senso-adventure-v2/">
+          <Image
+            src="/images/github.png"
+            width={30}
+            height={30}
+            className={styles.githubLogo}
+          />
+        </Link>
+      </span>
+      <div></div>
+    </div>
+  );
 };
 
 export default Footer;
