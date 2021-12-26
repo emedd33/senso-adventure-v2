482848c8bf3a70c4f49c7552bb1a9cc5ab505307
Eskild Mageli <eskild.emedd33@gmail.com>
2021-12-26 14:37:34 +0100

    fiks environment

--- utils/environment.ts
+++ utils/environment.ts
@@ -1,11 +1,3 @@
 export const getEnvironment = () => {
-  if (
-    window.location.hostname === "localhost" ||
-    window.location.hostname === "127.0.0.1" ||
-    window.location.hostname === ""
-  ) {
-    return "http://localhost:3000";
-  }
-
-  return window.location.host.toString();
+  return "https://www.sensoadventure.com";
 };
